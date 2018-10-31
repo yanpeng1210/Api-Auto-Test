191fe6ec7a92e572946f527952d630cafc337338
闫鹏github <476059272@qq.com>
2018-10-31 14:57:20 +0800

    新增片方 后台url

--- src/main/java/com/demo/common/GlobalVar.java
+++ src/main/java/com/demo/common/GlobalVar.java
@@ -9,7 +9,8 @@ public class GlobalVar {
 
     public static final String DOUBAN_MOVIE_SERVER = "DOUBAN_MOVIE_SERVER";
     public static String DOUBAN_MOVIE_SERVER_URL = "http://api.douban.com/v2/movie";
-
+    public static final String VIDEO_BMS_SERVER = "VIDEO_BMS_SERVER";
+    public static String VIDEO_BMS_SERVER_URL = "http://video-bms-api.mtime.com";
     public static Map<String, String> COOKIES = new HashMap<>();
     public static Map<String, Object> HEADERS = new HashMap<>();
 
