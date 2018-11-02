70176e96beeaa12e015414bc3c24132f4dfe726a
闫鹏github <476059272@qq.com>
2018-11-02 16:57:56 +0800

    新增票神URL

--- src/main/java/com/demo/common/GlobalVar.java
+++ src/main/java/com/demo/common/GlobalVar.java
@@ -11,6 +11,17 @@ public class GlobalVar {
     public static String DOUBAN_MOVIE_SERVER_URL = "http://api.douban.com/v2/movie";
     public static final String VIDEO_BMS_SERVER = "VIDEO_BMS_SERVER";
     public static String VIDEO_BMS_SERVER_URL = "http://video-bms-api.mtime.com";
+    /**
+     * 票神
+     */
+    public static String USER_API_PIAOSHEN_SERVER = "USER_API_PIAOSHEN_SERVER";
+    public static String USER_API_PIAOSHEN_URL = "http://user-api.piaoshen-dev.com";
+    public static String MISC_API_PIAOSHEN_SERVER = "MISC_API_PIAOSHEN_SERVER";
+    public static String MISC_API_PIAOSHEN_URL = "http://misc-api.piaoshen-dev.com";
+    public static String CINEMA_API_PIAOSHEN_SERVER = "CINEMA_API_PIAOSHEN_SERVER";
+    public static String CINEMA_API_PIAOSHEN_URL = "http://cinema-api.piaoshen-dev.com";
+    public static String TICKET_API_PIAOSHEN_SERVER = "TICKET_API_PIAOSHEN_SERVER";
+    public static String TICKET_API_PIAOSHEN_URL = "http://ticket-api.piaoshen-dev.com";
     public static Map<String, String> COOKIES = new HashMap<>();
     public static Map<String, Object> HEADERS = new HashMap<>();
 
